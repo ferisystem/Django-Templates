6c91806a8f1f96f95a78e6cb36041ed6dbc098b1
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-11-26 14:28:32 +0330

    Enriching a View with Data

--- trydjango/views.py
+++ trydjango/views.py
@@ -2,14 +2,21 @@
 To render html web pages
 """
 from django.http import HttpResponse
+import random
 
-HTML_STRING = """
-<h1>Hello World</h1>
-"""
 
 def home_view(request):
     """
     Take in a request (Django sends request)
     Return HTML as a response (We pick to return the response)
     """
+    number = random.randint(11, 666)
+    name = "Alireza"
+    H1_STRING = f"""
+    <h1>Hello {name} - {number}!</h1>
+    """
+    P_STRING = f"""
+    <p>Hi {name} - {number}!</p>
+    """
+    HTML_STRING = H1_STRING + P_STRING
     return HttpResponse(HTML_STRING)
