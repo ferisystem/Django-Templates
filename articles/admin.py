9a7665587745cd33e049d9fd69e3aeaa5d600900
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-11-27 02:03:00 +0330

    add articles to django admin panel

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    search_fields = ['title', 'content']
+
+admin.site.register(Article, ArticleAdmin)
