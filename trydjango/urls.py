6b12ad7f01e72b4a1027d707cef7c464a1a9240c
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-11-27 01:17:40 +0330

    Add articles dynamic URLs

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_view
+from articles.views import article_detail_view
 
 urlpatterns = [
     path('', home_view), # index / home / root
+    path('articles/<int:id>/', article_detail_view),
     path('admin/', admin.site.urls),
 ]
