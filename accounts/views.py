4a9b06e4f1f28c178918cdcb53f82f9b647733a4
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-11-30 20:07:15 +0330

    add sign-in & out & up funcs for handle request

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def signin_view(request):
+    if request.method == "POST":
+        username = request.POST.get("user")
+        password = request.POST.get("pass")
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            context = {"error": "Invalid username or password"}
+            return render(request, "accounts/signin.html", context=context)
+        login(request, user)
+        return redirect('/')
+    return render(request, "accounts/signin.html", {})
+
+
+def signout_view(request):
+    return render(request, "accounts/signout.html", {})
+
+
+def signup_view(request):
+    return render(request, "accounts/signout.html", {})
