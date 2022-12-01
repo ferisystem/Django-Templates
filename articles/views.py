cb634c92df33eb5248a9362e34cf449809c41c7a
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-12-01 21:00:48 +0330

    use django forms for creating articles process

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 from django.contrib.auth.decorators import login_required
+from .forms import ArticleForm
 
 # Create your views here.
 def article_search_view(request):
@@ -28,10 +29,13 @@ def article_search_view(request):
 
 @login_required
 def article_create_view(request):
-    context = {}
-    if request.method == "POST":
-        title = request.POST.get("title")
-        content = request.POST.get("content")
+    form = ArticleForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    if form.is_valid():
+        title = form.cleaned_data.get("title")
+        content = form.cleaned_data.get("content")
         article_obj = Article.objects.create(
             title = title,
             content = content,
