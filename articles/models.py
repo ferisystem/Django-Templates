4f4e7cff04cfd3a977a7e972c5456f1e200bf44f
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-12-06 02:49:54 +0330

    add publish model to Articles with allow to be null and blank in field

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Article(models.Model):
@@ -6,3 +7,9 @@ class Article(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    publish = models.DateField(
+        auto_now_add=False,
+        auto_now=False,
+        null=True,
+        blank=True
+    ) # you can use "default=timezone.now" too
