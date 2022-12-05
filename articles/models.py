8bcb2037d449d1c90205d72fa78f035553b9e30e
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-12-06 03:18:52 +0330

    add slug model

--- articles/models.py
+++ articles/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    slug = models.SlugField(null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     publish = models.DateField(
