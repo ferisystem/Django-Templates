ec6649f1824cd4b77d29ccec1f77111d1ea15fc3
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-12-06 02:47:00 +0330

    add updated model to Articles for admin panel

--- articles/models.py
+++ articles/models.py
@@ -5,3 +5,4 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
