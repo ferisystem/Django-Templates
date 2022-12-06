3416e8a978cde83b3ca67deb28294c8206916120
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-12-06 14:56:05 +0330

    use pre_save and post_save for saving/changing article models

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
+from django.db.models.signals import pre_save, post_save
+
 
 # Create your models here.
 class Article(models.Model):
@@ -17,6 +19,21 @@ class Article(models.Model):
     ) # you can use "default=timezone.now" too
 
     def save(self, *args, **kwargs):
-        if self.slug is None:
-            self.slug = slugify(self.title)
+        # if self.slug is None:
+            # self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+
+def article_pre_save(sender, instance, *args, **kwargs):
+    if instance.slug is None:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(article_pre_save, sender=Article)
+
+
+def article_post_save(sender, instance, created, *args, **kwargs):
+    if created:
+        instance.slug = slugify(instance.title)
+        instance.save()
+
+post_save.connect(article_post_save, sender=Article)
