bab9649e52b78209231dfcf821b69c081d996a3e
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-12-06 15:21:15 +0330

    show slug field in admin panel

--- articles/admin.py
+++ articles/admin.py
@@ -4,7 +4,7 @@ from .models import Article
 # Register your models here.
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'timestamp', 'updated']
+    list_display = ['id', 'title', 'slug', 'timestamp', 'updated']
     search_fields = ['title', 'content']
 
 admin.site.register(Article, ArticleAdmin)
