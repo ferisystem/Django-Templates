306ba43bc15fbec38d51eff4f6b7ebdb28fc4657
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-12-12 18:11:52 +0330

    fix bug articles/create url

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -29,9 +29,9 @@ from accounts.views import (
 
 urlpatterns = [
     path('', home_view), # index / home / root
+    path('articles/create/', article_create_view),
     path('articles/<slug:slug>/', article_detail_view),
     path('articles/', article_search_view),
-    path('articles/create/', article_create_view),
     path('admin/', admin.site.urls),
     path('login/', signin_view),
     path('logout/', signout_view),
