62d40e4ada49088c06d5b0e4f045194b24ec5c9c
Alireza Fereidouni <alirezafereidouni@proton.me>
2022-12-13 15:45:27 +0330

    add Recipe Models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Recipe(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
+    directions = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True) 
+    active = models.BooleanField(default=True)
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
+    quanity = models.CharField(max_length=50)  # 1 1/4
+    unit = models.CharField(max_length=50) # pounds, lbs, oz, gram, etc
+    directions = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True) 
+    active = models.BooleanField(default=True)
